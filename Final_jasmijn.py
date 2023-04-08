8359e3bda203b19e61b9697358a314b9b37ff1b9
Jasmijnva <125975375+Jasmijnva@users.noreply.github.com>
2023-04-08 20:12:54 +0200

    Update Final_jasmijn.py

--- Final_jasmijn.py
+++ Final_jasmijn.py
@@ -214,23 +214,23 @@ st.header('Een inzicht in de data verzameld over aardbevingen wereldwijd')
 st.caption('Bron: Kaggle (CHIRAG CHAUHAN)')
 
 with col1:
-st.plotly_chart(fig1)
+  st.plotly_chart(fig1)
 with col2:
-st.plotly_chart(fig2)
+  st.plotly_chart(fig2)
 with col1:
-st.plotly_chart(fig3)
+  st.plotly_chart(fig3)
 with col2:
-st.plotly_chart(fig4)
+  st.plotly_chart(fig4)
 with col1:
-st.pyplot(fig5)
+  st.pyplot(fig5)
 with col2:
-st.plotly_chart(fig6)
+  st.plotly_chart(fig6)
 with col1:
-st.plotly_chart(fig7)
+  st.plotly_chart(fig7)
 with col2:
-st.plotly_chart(fig8)
+  st.plotly_chart(fig8)
 with col1:
-st.plotly_chart(fig9)
+  st.plotly_chart(fig9)
 
 
 
