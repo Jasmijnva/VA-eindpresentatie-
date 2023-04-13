e399c04c8e40a1895a958e6cab4def1d386878f5
Jasmijnva <125975375+Jasmijnva@users.noreply.github.com>
2023-04-13 21:27:09 +0200

    Update Final_presentatie.py

--- Final_presentatie.py
+++ Final_presentatie.py
@@ -392,23 +392,20 @@ with tab3:
   col8, col9 = st.columns([250, 5])
   with col8: 
     st.plotly_chart(fig11)
-  
-  
-  
-  st.subheader('Conclusie accuratie van de magnitude vs rupture diepte')
-  st.write('Voor de accuratie van de magnitude hebben we een nieuwe kolom aangemaakt die het verschil laat zien tussen de estimated intensity en de reported intensity')
-  st.write('We hebben al gezien dat de magnitude correleert met de intensity van de aardbeving in de heatmap.') 
-  st.write('In deze plot is een trendline weergegeven die als R^2 een 0.019784 aangeeft. De R Squared geeft aan hoeveel van de variantie in de afhankelijk variabele verklaard wordt door de verklarende variabelen.')
-  st.write('In dit geval verklaart de variabele diepte voor 1,9% het verschil tussen de predicted en estimated intensity.')
-  st.write('Wat wel duidelijk is geworden uit deze plot is dat bij een minimale rupture diepte, de accuratie van de intensity slechter voorspeld is.')
+    st.subheader('Conclusie accuratie van de magnitude vs rupture diepte')
+    st.write('Voor de accuratie van de magnitude hebben we een nieuwe kolom aangemaakt die het verschil laat zien tussen de estimated intensity en de reported intensity')
+    st.write('We hebben al gezien dat de magnitude correleert met de intensity van de aardbeving in de heatmap.')
+    st.write('In deze plot is een trendline weergegeven die als R^2 een 0.019784 aangeeft. De R Squared geeft aan hoeveel van de variantie in de afhankelijk variabele verklaard wordt door de verklarende variabelen.')
+    st.write('In dit geval verklaart de variabele diepte voor 1,9% het verschil tussen de predicted en estimated intensity.')
+    st.write('Wat wel duidelijk is geworden uit deze plot is dat bij een minimale rupture diepte, de accuratie van de intensity slechter voorspeld is.')
  
   
   
 with col9:
   st.plotly_chart(fig7)
-   st.subheader('Conclusie alerts vs magnitude')
-   st.write('Het valt op dat de accuratie van de alerts afgegeven niet altijd even goed is.')
-   st.write('In 2010 was er een rood alert afgegeven voor een aarbeving met magnitude 7.2 terwijl in 2012 een groen alert was afgegeven voor een aardbeving van magnitude 8.2. Hier komen we later op terug wanneer we de volle maan gaan vergelijken met aardbevingen en tsunami s.')
+  st.subheader('Conclusie alerts vs magnitude')
+  st.write('Het valt op dat de accuratie van de alerts afgegeven niet altijd even goed is.')
+  st.write('In 2010 was er een rood alert afgegeven voor een aarbeving met magnitude 7.2 terwijl in 2012 een groen alert was afgegeven voor een aardbeving van magnitude 8.2. Hier komen we later op terug wanneer we de volle maan gaan vergelijken met aardbevingen en tsunami s.')
   
  
 with tab4:
